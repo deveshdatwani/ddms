2d34ab96abbf5fd6f5b9a3840ee8cb0634ba286f
deveshdatwani <datwanidevesh@gmail.com>
2026-01-13 22:35:57 -0500

    cleaning user app

--- user_app/celery_app.py
+++ user_app/celery_app.py
@@ -1,30 +1,12 @@
 import os
 from celery import Celery
-from PIL import Image
-from model.models.huggingface_cloth_segmentation.process import main as segment_apparel
 import logging
 
 logger = logging.Logger(__name__)
 logger.setLevel(logging.INFO)
 
+HOST = os.getenv("REDIS_HOST", "localhost")
 
-config = os.getenv("USER_APP_ENV", "prod")
-
-
-if config == "prod": 
-    HOST = "redis"
-else: 
-    HOST = "127.0.0.1"
-
-
-app = Celery("flask",
+app = Celery("celery_app",
              broker=f"redis://{HOST}:6379/0",
              backend=f"redis://{HOST}:6379/0")
-
-
-@app.task(name="tasks.infer")
-def segment_apparel_task(image_path):
-    image = Image.open(image_path)
-    image = segment_apparel(image)
-    image.save(image_path)
-    return True
